b189cdde81d378910604fb95a713928964b67bf6
mpodaniev <mpodanev@gmail.com>
2025-12-14 20:18:31 +0200

    create modular structure and dynamic data handling.

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,5 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) =>
+  posts.map(post => <PostInfo key={post.id} post={post} />);
+
